8b2d317521dd352e81a250f9468de35adb7ff299
petrero <petrero@gmail.com>
2017-01-29 17:57:16 +0200

    It is possible to create a new user from command line, the code is at vendor/friendsofsymfony/user-bundle/Command/CreateUserCommand.php.  You can infer from these lines that username, email and password are compulsory. How do we add these extra fields in the user form?  The extra FOSUserBundle fields were not automatically added when we created the CRUD using the command line. The automated CRUD creation process cannot pick up inheritance yet

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -5,6 +5,8 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
@@ -13,7 +15,19 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('firstname')->add('lastname')        ;
+        $builder
+          ->add('username')
+          ->add('email')      
+          ->add('firstname')
+          ->add('lastname') 
+          ->add('password', RepeatedType::class, array(
+            'type'              => PasswordType::class,
+            'invalid_message'   => 'The password fields must match.',
+            'required'          => true,
+            'first_options'     => array('label' => 'Password'),
+            'second_options'    => array('label' => 'Repeat Password')  
+          ))      
+        ;
     }
     
     /**
