515311e696fb9c34136f9433e3d41bc0697d35bc
petrero <petrero@gmail.com>
2017-01-30 18:19:52 +0200

    Add the setters and getters for the new modified and created values.   The schema is now changed and must be updated. Run in console 'bin/console doctrine:schema:update --dump-sql' and you will see what the sql is doing.   Once you are comfortable with that, force update it. Run in console 'bin/console doctrine:schema:update --force'

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -137,7 +137,53 @@ class User extends BaseUser
       }
       return $this;
     }
+    /**
+     * Set modified
+     *
+     * @param \DateTime $modified
+     *
+     * @return user
+     */
     
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
     
+    /**
+     * Get modified
+     *
+     * @return \DateTime
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return user
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
 }
 
