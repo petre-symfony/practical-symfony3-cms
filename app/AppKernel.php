0adc2db22be5bad5a14e55113bb7045821a64750
petrero <petrero@gmail.com>
2017-01-30 21:44:59 +0200

    Activate the required bundles in AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new AppBundle\User()
+            new AppBundle\User(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
