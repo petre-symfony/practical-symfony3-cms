479013885becf1f672188f170b1f78fff56f2d95
petrero <petrero@gmail.com>
2017-01-31 22:42:38 +0200

    Basically, we have created a checkUserRights function to ensure that other than the super admin, only the rightful owner can edit and see his own profile only.

---
+++ src/AppBundle/EventListener/AppSubscriber.php
@@ -0,0 +1,63 @@
+<?php
+namespace AppBundle\EventListener;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use JavierEguiluz\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+class AppSubscriber implements EventSubscriberInterface {
+  
+  protected $container;
+  
+  /**
+   * AppSubscriber constructor
+   * @param ContainerInterface $container
+   */
+  public function __construct(ContainerInterface $container) { //this is service container
+    $this->container = $container;
+  }
+  
+  /**
+   * @return array
+   */
+  public static function getSubscribedEvents(){
+    //return the subscribed events, their methods and priorities
+    return array(
+      EasyAdminEvents::PRE_LIST => 'checkUserRights',
+      EasyAdminEvents::PRE_EDIT => 'checkUserRights', 
+      EasyAdminEvents::PRE_SHOW => 'checkUserRights',  
+    );
+  }
+  
+  /**
+   * show an error if user is not superadmin and tries to manage restircted stuff
+   * @param GenericEvent $event event
+   * @return null
+   * @throws AccessDeniedException
+   */
+  
+  public function checkUserRights(GenericEvent $event){
+    //if super admin, show all
+    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_SUPER_ADMIN')){
+      return;
+    }
+    
+    $entity = $this->container->get('request_stack')->getCurrentRequest()->query->get('entity');
+    $action = $this->container->get('request_stack')->getCurrentRequest()->query->get('action');
+    $user_id = $this->container->get('request_stack')->getCurrentRequest()->query->get('id');
+    //if user management
+    if ($entity = 'User'){
+      //if edit and show
+      if ($action == 'edit' || $action == 'show'){
+        //check if himself
+        if ($user_id == $this->container->get('security.token_storage')->getToken()->getUser()->getId()){
+          return;
+        }
+      }
+    }
+    
+    //throw exception in all cases
+    throw new AccessDeniedException();
+  }
+}
