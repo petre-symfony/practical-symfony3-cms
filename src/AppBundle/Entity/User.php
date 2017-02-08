7dc170eb095b2f5c113448b3acee8a0d2a7f3af3
petrero <petrero@gmail.com>
2017-02-08 17:19:29 +0200

    Copy code from branch 12

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,172 @@
+<?php
+namespace AppBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255, nullable=true)
+     */
+    private $firstname;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastname", type="string", length=255, nullable=true)
+     */
+    private $lastname;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $modified;
+    
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+    
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist(){
+      //update the modified time
+      $this->setModified(new \DateTime());
+      
+      //for newly created entries
+      if ($this->getCreated() == null){
+        $this->setCreated(new \DateTime('now'));
+      }
+    }
+    
+    /**
+     * @ORM\PreUpdate
+     */
+    public function preUpdate(){
+      //update the modified time
+      $this->setModified(new \DateTime());
+    }
+    
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+    /**
+     * Override parent's method. Don't set passwd if its null
+     * 
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword($password){
+      if ($password){
+        $this->password = $password;
+      }
+      return $this;
+    }
+    /**
+     * Set modified
+     *
+     * @param \DateTime $modified
+     *
+     * @return user
+     */
+    
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+        return $this;
+    }
+    
+    /**
+     * Get modified
+     *
+     * @return \DateTime
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return user
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+        return $this;
+    }
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
