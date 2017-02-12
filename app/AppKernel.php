dc3ea99cbaf894ad97e14cb2d0a3f02abce9d1b9
petrero <petrero@gmail.com>
2017-02-12 13:28:59 +0200

    Add a few parameters in config.yml and in AppKernel.php. Create folder uploads/profiles in web directory and ignore it in .gitignore.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\User(),
-            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle()
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
