9705c220f56b18c0b326431f34234123b56318b8
petrero <petrero@gmail.com>
2017-02-21 21:09:35 +0200

    Create a new service to subscribe to the kernel.request event.

--- src/AppBundle/EventListener/AppSubscriber.php
+++ src/AppBundle/EventListener/AppSubscriber.php
@@ -4,11 +4,14 @@ use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 use JavierEguiluz\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use FOS\UserBundle\FOSUserEvents;
 use FOS\UserBundle\Event\FormEvent;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use AppBundle\Entity\UserLog;
 
 class AppSubscriber implements EventSubscriberInterface {
   
@@ -31,7 +34,8 @@ class AppSubscriber implements EventSubscriberInterface {
       EasyAdminEvents::PRE_LIST => 'checkUserRights',
       EasyAdminEvents::PRE_EDIT => 'checkUserRights', 
       EasyAdminEvents::PRE_SHOW => 'checkUserRights', 
-      FOSUserEvents::RESETTING_RESET_SUCCESS => 'redirectUserAfterPasswordReset'  
+      FOSUserEvents::RESETTING_RESET_SUCCESS => 'redirectUserAfterPasswordReset',
+      KernelEvents::REQUEST => 'onKernelRequest' 
     );
   }
   
@@ -77,4 +81,37 @@ class AppSubscriber implements EventSubscriberInterface {
     $url = $this->container->get('router')->generate('admin');
     $event->setResponse(new RedirectResponse($url));
   }
+  
+  
+  /** 
+   * 
+   * @param Configure $event GetResponseUserEvent
+   * @return null
+   */
+  
+  public function onKernelRequest(GetResponseEvent $event){
+    $request = $event->getRequest();
+    $current_url = $request->server->get('REQUEST_URI');
+    //ensures we track admin only
+    $admin_path = $this->container->getParameter('admin_path');
+    
+    //only log admin area and only if user is logged in. Don't log search by filter
+    if (!is_null($this->container->get('security.token_storage')->getToken()) 
+        && preg_match('/\/'.$admin_path.'\//', $current_url) 
+        && ($request->query->get('filter') === null) 
+        && !preg_match('/\/userlog\//', $current_url)){
+      $em= $this->container->get('doctrine.orm.entity_manager');
+      $log = new UserLog();
+      $log->setData(json_encode($request->request->all()));
+      $log->setUsername($this->container->get('security.token_storage')->getToken()->getUser()->getUsername());
+      $log->setCurrentUrl($current_url);
+      $log->setReferrer($request->server->get('HTTP_REFERER'));
+      $log->setAction($request->getMethod());
+      $log->setCreated(new \DateTime('now'));
+      $em->persist($log);
+      $em->flush();
+    }
+  }
 }
+
+
