9e444f599167eb6f216f54e46e8718d86594a747
petrero <petrero@gmail.com>
2017-02-21 19:54:26 +0200

    Copy code from branch Chp14-UploadingFiles

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use JavierEguiluz\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminController extends BaseAdminController {
+  
+  /**
+   * @Route("/dashboard", name = "dashboard")
+   * 
+   * @param Request $request
+   * @return \Symfony\Component\HttpFoundation\Response
+   * 
+   */
+  public function dashboardAction(Request $request){
+    return $this->render('easy_admin/dashboard.html.twig');
+  }
+  
+  public function  createNewUserEntity(){
+    return $this->get('fos_user.user_manager')->createUser();
+  }
+  
+  public function prePersistUserEntity($user) {
+    $this->get('fos_user.user_manager')->updateUser($user, false);  
+  }
+  
+  public function preUpdateUserEntity($user) {
+    $this->get('fos_user.user_manager')->updateUser($user, false);
+  }
+  
+  /** 
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function showUserAction(){
+    $this->dispatch(EasyAdminEvents::PRE_SHOW);
+    $id = $this->request->query->get('id');
+    $easyadmin = $this->request->attributes->get('easyadmin');
+    $entity = $easyadmin['item'];
+    
+    $fields = $this->entity['show']['fields'];
+    
+    if(!$this->isGranted('ROLE_SUPER_ADMIN')){
+      unset($fields['created']);
+    }
+    
+    $deleteForm = $this->createDeleteForm($this->entity['name'], $id);
+    
+    return $this->render($this->entity['templates']['show'], array(
+        'entity' => $entity,
+        'fields' => $fields,
+        'delete_form'=> $deleteForm->createView()
+    ));
+  }
+  
+  /**
+   * The method that is executed when the user performs a 'edit' action on entity
+   * 
+   * @return RedirectResponse|Response
+   */
+  protected function editUserAction(){
+    $this->dispatch(EasyAdminEvents::PRE_EDIT);
+    $id = $this->request->query->get('id');
+    $easyadmin = $this->request->attributes->get('easyadmin');
+    $entity = $easyadmin['item'];
+    
+    if($this->request->isXmlHttpRequest() && $property = $this->request->query->get('property')){
+      $newValue = 'true' === strtolower($this->request->query->get('newValue'));
+      $fieldsMetadata = $this->entity['list']['fields'];
+      
+      if(!isset($fieldsMetadata[$property]) || 'toggle' !== $fieldsMetadata[$property]['dataType']){
+        throw new \RuntimeException(sprintf('The type of the "%s" property is not "toggle".', $property));
+      }
+      
+      $this->updateEntityProperty($entity, $property, $newValue);
+      
+      return new Response((string)$newValue);
+    }
+    
+    $fields = $this->entity['edit']['fields'];
+    
+    $editForm = $this->createEditForm($entity, $fields);
+    if (!$this->isGranted('ROLE_SUPER_ADMIN')){
+      $editForm->remove('enabled');
+      $editForm->remove('roles');
+      $editForm->remove('locked');
+      $editForm->remove('expired');
+    }
+    
+    $deleteForm = $this->createDeleteForm($this->entity['name'], $id);
+    
+    $editForm->handleRequest($this->request);
+    if($editForm->isValid()){
+      $this->preUpdateUserEntity($entity);
+      $this->em->flush();
+      
+      $refererUrl = $this->request->query->get('referer', '');
+      
+      return !empty($refererUrl) ? $this->redirect($refererUrl) : $this->redirect($this->generateUrl('easyadmin', array('action' => 'show', 'entity' => $this->entity['name'], 'id' => $id)));
+    }
+    
+    return $this->render($this->entity['templates']['edit'], array(
+      'form' => $editForm->createView(),
+      'entity_fields' => $fields,
+      'entity' => $entity,
+      'delete_form' => $deleteForm->createView()  
+    ));
+  }
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,221 @@
+<?php
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255, nullable=true)
+     */
+    private $firstname;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastname", type="string", length=255, nullable=true)
+     */
+    private $lastname;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $modified;
+    
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+    
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image = '';
+    
+    /**
+     * @Vich\UploadableField(mapping="profile_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+    
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist(){
+      //update the modified time
+      $this->setModified(new \DateTime());
+      
+      //for newly created entries
+      if ($this->getCreated() == null){
+        $this->setCreated(new \DateTime('now'));
+      }
+    }
+    
+    /**
+     * @ORM\PreUpdate
+     */
+    public function preUpdate(){
+      //update the modified time
+      $this->setModified(new \DateTime());
+    }
+    
+    /**
+     * @param File|null $image
+     */
+    public function setImageFile(File $image=null){
+      $this->imageFile = $image;
+      //at least 1 field needs to change for doctrine to save
+      if ($image){
+        $this->setModified(new \DateTime());
+      }
+    }
+    
+    /**
+     * @return File
+     */
+    public function getImageFile(){
+      return $this->imageFile;
+    }
+    
+    /** 
+     * @param $image
+     */
+    public function setImage($image){
+      $this->image = $image;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getImage() {
+      return $this->image;
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+    /**
+     * Override parent's method. Don't set passwd if its null
+     * 
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword($password){
+      if ($password){
+        $this->password = $password;
+      }
+      return $this;
+    }
+    /**
+     * Set modified
+     *
+     * @param \DateTime $modified
+     *
+     * @return user
+     */
+    
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+        return $this;
+    }
+    
+    /**
+     * Get modified
+     *
+     * @return \DateTime
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return user
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+        return $this;
+    }
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
